8484f5ba256c73d0ecce6f3bc95ac5c00fb1d287
Kaotus <113468981+Kaotus@users.noreply.github.com>
2026-05-16 09:12:35 +0200

    Add reel upload studio UI

---
+++ src/components/UploadStudio.jsx
@@ -0,0 +1,12 @@
+export default function UploadStudio() {
+  return (
+    <div className='uploadStudio'>
+      <h2>Create Reel</h2>
+
+      <div className='uploadBox'>
+        <span>Drag & Drop Video</span>
+        <button>Upload Reel</button>
+      </div>
+    </div>
+  )
+}
