f8cef0959c726c3c7c56ceb0694297e5ef0f5493
Kaotus <113468981+Kaotus@users.noreply.github.com>
2026-05-16 09:04:46 +0200

    Add futuristic profile card

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,14 @@
+export default function ProfileCard() {
+  return (
+    <div className='profileCard'>
+      <div className='avatar'></div>
+
+      <div>
+        <h3>Kaotus</h3>
+        <span>AI Curated Community Feed</span>
+      </div>
+
+      <button className='followBtn'>Follow</button>
+    </div>
+  )
+}
